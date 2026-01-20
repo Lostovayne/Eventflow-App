528c7523ef2b2689938eafaf13699185a71df85e
Deus lo Vult <92962731+Lostovayne@users.noreply.github.com>
2026-01-20 02:10:09 -0300

    Add DB-backed user registration and Kafka event

--- apps/auth-service/src/auth-service.service.ts
+++ apps/auth-service/src/auth-service.service.ts
@@ -1,11 +1,17 @@
+import { DatabaseService, users } from '@app/database';
 import { KAFKA_SERVICE, KAFKA_TOPICS } from '@app/kafka';
 import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { ClientKafka } from '@nestjs/microservices';
+import { eq } from 'drizzle-orm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthServiceService implements OnModuleInit {
   constructor(
     @Inject(KAFKA_SERVICE) private readonly kafkaClient: ClientKafka,
+    private readonly dbService: DatabaseService,
+    private readonly jwtService: JwtService,
   ) {}
 
   async onModuleInit() {
@@ -13,16 +19,37 @@ export class AuthServiceService implements OnModuleInit {
     await this.kafkaClient.connect();
   }
 
-  getHello(): string {
-    return 'Hello World!';
-  }
+  async register(email: string, password: string, name: string) {
+    // Check if user already exists
+    const existingUser = await this.dbService.db
+      .select()
+      .from(users)
+      .where(eq(users.email, email))
+      .limit(1);
 
-  simulateUserRegistration(email: string) {
+    if (existingUser.length > 0) {
+      throw new Error('User already exists');
+    }
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    // create new user
+    const [user] = await this.dbService.db
+      .insert(users)
+      .values({
+        email,
+        password: hashedPassword,
+        name,
+      })
+      .returning();
+
+    // Send user registred event to Kafka
     this.kafkaClient.emit(KAFKA_TOPICS.USER_REGISTERED, {
-      email,
+      userId: user.id,
+      email: user.email,
       timestamp: new Date().toISOString(),
     });
 
-    return { message: 'User registration event emitted', email };
+    return { message: 'User registered successfully', userId: user.id };
   }
 }
